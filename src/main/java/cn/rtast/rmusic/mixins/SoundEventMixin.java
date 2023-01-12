21efb236841b7687e68216c50e44e5ec48c84256
RTAkland <markushammered@gmail.com>
2023-01-12 18:01:11 +0800

    update: 更新注释

--- src/main/java/cn/rtast/rmusic/mixins/SoundEventMixin.java
+++ src/main/java/cn/rtast/rmusic/mixins/SoundEventMixin.java
@@ -18,13 +18,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SoundSystem.class)
 public class SoundEventMixin {
+    // 如果当前正在播放音乐取消播放游戏背景音乐
     @Inject(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At("HEAD"), cancellable = true)
     public void sound(SoundInstance sound, CallbackInfo ci) {
         if (RMusicClient.INSTANCE.getPlayer() != null) {
             if (RMusicClient.INSTANCE.getPlayer().getStatus() == Status.PLAYING) {
                 SoundCategory category = sound.getCategory();
                 if (category == SoundCategory.MUSIC) {
-                    ci.cancel();  // 如果音乐播放器正在播放音乐取消播放背景音乐
+                    ci.cancel();
                 }
             }
         }
