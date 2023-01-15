f46df3ebddbce249518e395cc010a202a87b81cc
RTAkland <markushammered@gmail.com>
2023-01-15 14:11:43 +0800

    update: 更改为基类被继承

--- src/main/kotlin/cn/rtast/rmusic/commands/RMusicCommand.kt
+++ src/main/kotlin/cn/rtast/rmusic/commands/RMusicCommand.kt
@@ -6,51 +6,119 @@
 
 package cn.rtast.rmusic.commands
 
-import cn.rtast.rmusic.common.command.IRMusicCommand
 import cn.rtast.rmusic.network.S2CPacket
-import cn.rtast.rmusic.utils.SearchUtil
+import com.mojang.brigadier.CommandDispatcher
+import com.mojang.brigadier.arguments.DoubleArgumentType.doubleArg
+import com.mojang.brigadier.arguments.DoubleArgumentType.getDouble
+import com.mojang.brigadier.arguments.IntegerArgumentType.getInteger
+import com.mojang.brigadier.arguments.IntegerArgumentType.integer
+import com.mojang.brigadier.arguments.StringArgumentType.getString
+import com.mojang.brigadier.arguments.StringArgumentType.string
 import com.mojang.brigadier.context.CommandContext
+import net.minecraft.server.command.CommandManager.argument
+import net.minecraft.server.command.CommandManager.literal
 import net.minecraft.server.command.ServerCommandSource
 
-class RMusicCommand : IRMusicCommand {
+open class RMusicCommand{
+    fun register(dispatcher: CommandDispatcher<ServerCommandSource>) {
+        val literalBuilder = literal("rmusic")
+            .requires { it.hasPermissionLevel(0) }
+            .then(
+                literal("play")
+                    .then(
+                        argument("id", integer())
+                            .executes { play(it, getInteger(it, "id"));1 }
+                    )
+            )
+            .then(
+                literal("pause")
+                    .executes { pause(it);1 }
+            )
+            .then(
+                literal("resume")
+                    .executes { resume(it);1 }
+            )
+            .then(
+                literal("mute")
+                    .executes { mute(it);1 }
+            )
+            .then(
+                literal("volume")
+                    .then(
+                        argument("volume", doubleArg())
+                            .executes { setVolume(it, getDouble(it, "volume"));1 }
+                    )
+            )
+            .then(
+                literal("stop")
+                    .executes { stop(it);1 }
+            )
+            .then(
+                literal("search")
+                    .then(
+                        argument("kwd", string())
+                            .executes { search163(it, getString(it, "kwd"));1 }
+                    )
+            )
+            .then(
+                literal("login")
+                    .then(
+                        literal("163")
+                            .then(
+                                argument("email", string())
+                                    .then(
+                                        argument("pwd", string())
+                                            .executes {
+                                                login163(it, getString(it, "email"), getString(it, "pwd"));1
+                                            }
+                                    )
+                            )
+                    )
+            )
+            .then(
+                literal("logout")
+                    .executes { logout163(it);1 }
+            )
+        dispatcher.register(literalBuilder)
+    }
 
     private fun send(cmd: Int, body: String, ctx: CommandContext<ServerCommandSource>) {
         S2CPacket().send("$cmd]$body", ctx.source.player!!)
     }
 
-    override fun play(ctx: CommandContext<ServerCommandSource>, id: Int) {
-        send(0, id.toString(), ctx)
+    open fun play(ctx: CommandContext<ServerCommandSource>, id: Int) {
+        send(0, "163^$id", ctx)
     }
 
-    override fun stop(ctx: CommandContext<ServerCommandSource>) {
-        send(1, "stop", ctx)
-    }
-
-    override fun resume(ctx: CommandContext<ServerCommandSource>) {
-        send(2, "resume", ctx)
-    }
-
-    override fun pause(ctx: CommandContext<ServerCommandSource>) {
+    open fun pause(ctx: CommandContext<ServerCommandSource>) {
         send(3, "pause", ctx)
     }
 
-    override fun mute(ctx: CommandContext<ServerCommandSource>) {
+    open fun resume(ctx: CommandContext<ServerCommandSource>) {
+        send(2, "resume", ctx)
+    }
+
+    open fun mute(ctx: CommandContext<ServerCommandSource>) {
         send(4, "mute", ctx)
     }
 
-    override fun setVolume(ctx: CommandContext<ServerCommandSource>, value: Double) {
+    open fun setVolume(ctx: CommandContext<ServerCommandSource>, value: Double) {
         send(5, value.toString(), ctx)
     }
 
-    override fun searchNetease(ctx: CommandContext<ServerCommandSource>, keyword: String) {
-        send(8, "$keyword", ctx)
+    open fun stop(ctx: CommandContext<ServerCommandSource>) {
+        send(1, "stop", ctx)
     }
 
-    override fun loginNetease(ctx: CommandContext<ServerCommandSource>, email: String, password: String) {
-        send(6, "$email^$password", ctx)
+    open fun search163(ctx: CommandContext<ServerCommandSource>, keyword: String) {
+        send(8, "163^$keyword", ctx)
     }
 
-    override fun logoutNetease(ctx: CommandContext<ServerCommandSource>) {
-        send(7, "logout", ctx)
+    open fun login163(ctx: CommandContext<ServerCommandSource>, email: String, password: String) {
+        send(6, "163^$email^$password", ctx)
+    }
+
+    open fun logout163(ctx: CommandContext<ServerCommandSource>) {
+        send(7, "163^logout", ctx)
     }
 }
