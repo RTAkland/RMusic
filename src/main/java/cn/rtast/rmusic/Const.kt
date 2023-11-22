035d9b0a148ac2f117df7bd13fe1211a53f2efd7
RTAkland <rtakland@outlook.com>
2023-11-22 13:10:16 +0800

    update const

--- src/main/java/cn/rtast/rmusic/Const.kt
+++ src/main/java/cn/rtast/rmusic/Const.kt
@@ -19,6 +19,7 @@ package cn.rtast.rmusic
 
 const val VERSION = "0.1.0b"
 const val DEFAULT_CONF_PATH = "./config/rmusic"
+const val LOG_FILE_PATH = "./logs/"
 
 const val CELLPHONE_LOGIN_PATH = "/login/cellphone"
 const val EMAIL_LOGIN_PATH = "/login"
@@ -26,9 +27,12 @@ const val EMAIL_LOGIN_PATH = "/login"
 const val QRCODE_KEY_GEN_PATH = "/login/qr/key"
 const val QRCODE_GEN_PATH = "/login/qr/create"
 const val QRCODE_CHECK_PATH = "/login/qr/check"
-const val ANONIMOUS_PATH = "/register/anonimous"
-const val LOGIN_REFRESH = "/login/refresh"
 const val CAPTCHA_SEND_PATH = "/captcha/sent"
 const val VERIFY_CAPTCHA_PATH = "/captcha/verify"
+const val SONG_URL_V1_PATH = "/song/url/v1"
 
+const val WHITE_ANSI_PREFIX = "\u001B[37m"
+const val GREEN_ANSI_PREFIX = "\u001B[32m"
+const val YELLOW_ANSI_PREFIX = "\u001B[33m"
+const val RED_ANSI_PREFIX = "\u001B[31m"
 
